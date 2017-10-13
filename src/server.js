3a61d42e0f8e71b80b9b635cc4ecf64f1ec892ce
Clayton <cscavone96@gmail.com>
2017-10-12 21:42:47 -0400

    Fixed issues

--- src/server.js
+++ src/server.js
@@ -18,42 +18,45 @@ console.log(`Listening on 127.0.0.1: ${port}`);
 
 const io = socketio(app);
 
-let blocks = [];
-
-let block = {};
+const blocks = [];
 
 const users = [];
 
 const points = {};
 
-let draws = {};
+const draws = {};
 
-let difficulty = 0.92;
+const difficulty = 0.92;
 
 let blockCount = 0;
 
-const emitBlock = () => { //Emits new random block to all players
+const emitBlock = () => { // Emits new random block to all players
   let newBlock = {};
-  const xPos = Math.floor((Math.random() * 500))+6;
+  const xPos = Math.floor((Math.random() * 500)) + 6;
   const xSize = Math.floor((Math.random() * (50 - 25)) + 25);
   const ySize = Math.floor((Math.random() * (50 - 25)) + 25);
   const yPos = 0 - ySize;
-  let newSpeed = Math.floor((Math.random() * 3)) + 1;
-  newBlock = { x: xPos, y: yPos, width: xSize, height: ySize, isBlock: blockCount, speed: newSpeed};
+  const newSpeed = Math.floor((Math.random() * 3)) + 1;
+  newBlock = { x: xPos,
+    y: yPos,
+    width: xSize,
+    height: ySize,
+    isBlock: blockCount,
+    speed: newSpeed };
   blockCount++;
   blocks.push(newBlock);
-  let keys = Object.keys(blocks);        
-  for(let i = 0; i < keys.length; i++){
-  newBlock = blocks[keys[i]];
-  draws[newBlock.isBlock] = newBlock; 
+  const keys = Object.keys(blocks);
+  for (let i = 0; i < keys.length; i++) {
+    newBlock = blocks[keys[i]];
+    draws[newBlock.isBlock] = newBlock;
   }
 };
 
-const deleteBlock = (block) =>{ //Removes block from list
-        blocks.pop(block);
+const deleteBlock = (block) => { // Removes block from list
+  blocks.pop(block);
 };
 
-const emitPlayers = () => { //Emits player list and scores to players
+const emitPlayers = () => { // Emits player list and scores to players
   let data = '';
   users.forEach((element) => {
     data = `${data}<li>${element.name} - ${points[element.name]}</li>`;
@@ -62,87 +65,79 @@ const emitPlayers = () => { //Emits player list and scores to players
 };
 
 
-const onJoined = (sock) => { //Handles player joining and rejects duplicate names
-    const socket = sock;
-    let nameTaken = false;
-    socket.on('join', (data) => 
-    {
-        for(var i = 0; i < users.length; i++){
-            if(users[i].name == data.name){
-                nameTaken = true;
-            }
-        }
-        if(!nameTaken)
-        {
-            socket.name = data.name;
-            users.push(socket);
-            socket.join('room1');
-            points[socket.name] = 0;
-            console.log(`${data.name} joined`);
-            emitPlayers(socket);
-            socket.emit('onJoined', data);
-            socket.on('disconnect', () => {
-                console.log(users);
-                const userInd = users.indexOf(socket.name);
-                users.splice(userInd, 1);  
-                emitPlayers(socket);
-                delete draws[socket.name];
-                socket.leave('room1');
-            });
-        }
-        else{
-            socket.name = "NAMETAKEN"
-            socket.emit('nameTaken');
-            socket.disconnect();
-        }      
-    });
-};
-
-const resetPoints = (data) =>{ //Sets points of player to zero
-    points[data.name] = 0;
-    io.sockets.in('room1').emit('setBGColor', data.color);    
-};
-
-const update = () => { //Updates draw/game data for all connected clients
-    let r = Math.random();
-    const time = new Date().getTime();
-    let keys = Object.keys(draws);
-    let drawData = [];
-    if(users.length > 0){
-        if(r < 1-difficulty)
-        {
-            emitBlock();
-        }
+const onJoined = (sock) => { // Handles player joining and rejects duplicate names
+  const socket = sock;
+  let nameTaken = false;
+  socket.on('join', (data) => {
+    for (let i = 0; i < users.length; i++) {
+      if (users[i].name === data.name) {
+        nameTaken = true;
+      }
     }
-    for(let i = 0; i < keys.length; i++){
-        const drawCall = draws[keys[i]];
-        draws[keys[i]].lastUpdate = time;
-        if(draws[keys[i]].hasOwnProperty("isBlock"))
-        {
-            draws[keys[i]].y += draws[keys[i]].speed;
-        }
-        if (draws[keys[i]].y >= 550){
-            deleteBlock(keys[i]);
-            delete draws[keys[i]];
-            users.forEach((element) => {
-                points[element.name] = points[element.name] + 1;
-            });
-        }
-        else{
-            let newDraw = {name: keys[i], coords: draws[keys[i]]};
-            drawData.push(newDraw);
-            draws[newDraw.name] = newDraw.coords;
-        }
-    }   
-    emitPlayers();
-    io.sockets.in('room1').emit('drawUpdate', drawData);    
-};      
+    if (!nameTaken) {
+      socket.name = data.name;
+      users.push(socket);
+      socket.join('room1');
+      points[socket.name] = 0;
+      console.log(`${data.name} joined`);
+      emitPlayers(socket);
+      socket.emit('onJoined', data);
+      socket.on('disconnect', () => {
+        console.log(users);
+        const userInd = users.indexOf(socket.name);
+        users.splice(userInd, 1);
+        emitPlayers(socket);
+        delete draws[socket.name];
+        socket.leave('room1');
+      });
+    } else {
+      socket.name = 'NAMETAKEN';
+      socket.emit('nameTaken');
+      socket.disconnect();
+    }
+  });
+};
 
-io.sockets.on('connection', (socket) => { //Listens for client emits
+const resetPoints = (data) => { // Sets points of player to zero
+  points[data.name] = 0;
+  io.sockets.in('room1').emit('setBGColor', data.color);
+};
+
+const update = () => { // Updates draw/game data for all connected clients
+  const r = Math.random();
+  const time = new Date().getTime();
+  const keys = Object.keys(draws);
+  const drawData = [];
+  if (users.length > 0) {
+    if (r < 1 - difficulty) {
+      emitBlock();
+    }
+  }
+  for (let i = 0; i < keys.length; i++) {
+    draws[keys[i]].lastUpdate = time;
+    if (draws[keys[i]].isBlock != -1) {
+      draws[keys[i]].y += draws[keys[i]].speed;
+    }
+    if (draws[keys[i]].y >= 550) {
+      deleteBlock(keys[i]);
+      delete draws[keys[i]];
+      users.forEach((element) => {
+        points[element.name]++;
+      });
+    } else {
+      const newDraw = { name: keys[i], coords: draws[keys[i]] };
+      drawData.push(newDraw);
+      draws[newDraw.name] = newDraw.coords;
+    }
+  }
+  emitPlayers();
+  io.sockets.in('room1').emit('drawUpdate', drawData);
+};
+
+io.sockets.on('connection', (socket) => { // Listens for client emits
   onJoined(socket);
   socket.on('blockClicked', (name) => {
     points[name]++;
-    block = {};
     emitBlock(socket);
     emitPlayers(socket);
   });
